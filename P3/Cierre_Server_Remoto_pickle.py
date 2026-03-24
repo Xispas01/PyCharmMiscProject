ac54b308e104cffcc68223ed3cf0544cfa2b6c29
Jose Gimeno Niza <JGIMNIZ@upv.edu.es>
2026-03-24 10:25:07 +0100

    Cierres remotos para version normal y con pickle

---
+++ P3/Cierre_Server_Remoto_pickle.py
@@ -0,0 +1,20 @@
+# Importar la biblioteca de sockets
+import socket
+import pickle
+
+if __name__ == '__main__':
+    # Creamos un objeto socket TCP
+    conexion = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Definimos el puerto y la dirección IP del servidor al que nos conectaremos
+    puerto = 12345
+    direccion_ip = "127.0.0.1"
+    # Establecemos una conexión al servidor
+    conexion.connect((direccion_ip, puerto))
+    # Enviamos un mensaje al servidor
+    message = "CERRAR"
+    conexion.send(pickle.dumps(message))
+    # Recibimos una respuesta del servidor
+    respuesta = conexion.recv(1024)
+    print("Respuesta recibida:", respuesta.decode())
+    # Cerramos la conexión
+    conexion.close()
