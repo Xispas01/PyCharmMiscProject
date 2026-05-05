edcca171356125dab99111f40b13a75f1ecdf1e4
Jose Gimeno Niza <JGIMNIZ@upv.edu.es>
2026-05-05 10:21:21 +0200

    Base Robots

--- Trabajo/publicador.py
+++ Trabajo/publicador.py
@@ -6,15 +6,24 @@ import sys
 
 THE_BROKER = "localhost"
 BROKER_PORT = 1883
-ROOT = "$SYS/"
-topic1 = ROOT+"sensor/datoA"
+ID = 1
+
+ROOT = "$Planta/"
+position = ROOT+f"robots/{ID:03d}/position"
+target_pos = ROOT+f"robots/{ID:03d}/target_pos"
+battery = ROOT+f"robots/{ID:03d}/battery"
+
+topicList = [position,target_pos,battery]
+topicConfig = {position:(0,False),target_pos:(1,True),battery:(0,False)}
 
 if __name__ == '__main__':
-    client = Client(client_id="PubTest")
+    client = Client(client_id="Robot_[ID]")
     client.connect(THE_BROKER, BROKER_PORT)
 
     while True:
-        lectura = random.randint(1,10)
-        print("publicando dato sensor/datoA")
-        client.publish(topic1,lectura)
-        time.sleep(0.5)
+        print("publicando datos")
+        for t in topicList:
+            lectura = random.randint(1,10)
+            cfg = topicConfig[t]
+            client.publish(t,lectura,cfg[0],cfg[1])
+        time.sleep(1)
